dc41c564f275db72a0530b351e6f6c3c3963c255
mazharrasyad <muhazharrasyad@gmail.com>
2019-01-08 16:58:06 +0700

    Finishing

--- PencarianKataArgument.java
+++ PencarianKataArgument.java
@@ -1,56 +1,62 @@
 import java.util.Scanner;
 import java.io.File;
-import java.io.PrintWriter;
-import java.io.FileNotFoundException;
 
 public class PencarianKataArgument
 {
     public static void main(String args[])
-    {   
-        Scanner scan = new Scanner(System.in);        
+    {                   
+        if(args.length == 0)
+        {
+            System.out.println("========================");     
+            System.out.println("|| Argument Tidak Ada ||");     
+            System.out.println("========================"); 
+        }
+        else if (args.length == 1)
+        {
+            System.out.println("====================");     
+            System.out.println("|| File Tidak Ada ||");     
+            System.out.println("===================="); 
+        }
+        else
+        {       
+            System.out.println("Kata yang dicari : " + args[0]);                
+            System.out.println();                            
 
-        System.out.print("Kata yang dicari : " + args[0] + "\n\n");                
-
-        if (1 < args.length)
-        {         
             for(int i = 1; i < args.length; i++)
             {                    
-                String inputFileName = args[i];
-
                 try
-                {
-                    File inputFile = new File(inputFileName);
-                    Scanner scanfile = new Scanner(inputFile);
-                    int counter = 0;
-
-                    while(scanfile.hasNext())
-                    {           
-                        String input = scanfile.next();
+                {                                
+                    if(args[i].substring(args[i].length() - 4).equals(".txt")){                                                                
+                        File namafile = new File(args[i]);
+                        Scanner scanfile = new Scanner(namafile);
+                        int jumlah = 0;
                         
-                        if(input.equalsIgnoreCase(args[0])){                    
-                            counter++;                                       
-                        }                                           
-                    }
-                    if (counter == 0){                    
-                        System.out.println("Kata " + args[0] + " pada file " + args[i] + " tidak ada");
-                    }       
+                        while(scanfile.hasNext())
+                        {           
+                            String kata = scanfile.next();
+                            
+                            if(kata.equalsIgnoreCase(args[0])){                    
+                                jumlah++;                                       
+                            }                                           
+                        }
+                        scanfile.close();
+
+                        if (jumlah == 0){                    
+                            System.out.println("Pada file " + args[i] + " tidak ada");
+                        }       
+                        else{
+                            System.out.println("Pada file " + args[i] + " ada " + jumlah);
+                        }   
+                    }                   
                     else{
-                        System.out.println("Kata " + args[0] + " pada file " + args[i] + " ada " + counter);
-                    }                        
-                    scanfile.close();
-                    scan.close();
+                        System.out.println("File " + args[i] + " tidak berekstensi .txt");                        
+                    }       
                 }
                 catch(Exception e)
                 { 
                     System.out.println("File " + args[i] + " tidak ada");    
                 }                
             }                        
-        }
-        else
-        {
-            System.out.println("===========================");     
-            System.out.println("|| File Tidak Dimasukkan ||");     
-            System.out.println("==========================="); 
-        }
+        }        
     }
 }
